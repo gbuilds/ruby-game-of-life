1b41ddbc8834e418fedd68db24f620d324e68279
Gordie Smith <agsmith4@gmail.com>
2015-04-15 20:04:27 +0000

    Add context 'Rules' in spec

--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -47,4 +47,18 @@ describe "Game of Life" do
       expect(subject.y).to eq 0
     end
   end
+    
+  context "Rules" do
+    context "Rule 1: Any live cell with fewer than two live neighbours dies, as if caused by under-population" do
+    end
+    
+    context "Rule 2: Any live cell with two or three live neighbours lives on to the next generation" do
+    end
+    
+    context "Rule 3: Any live cell with more than three live neighbours dies, as if by overcrowding" do
+    end
+    
+    context "Rule 4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction" do
+    end
+  end
 end
