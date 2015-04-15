5029cc22660d11c5fce89ed307bddca352888b42
Gordie Smith <agsmith4@gmail.com>
2015-04-15 18:49:41 +0000

    Create world class

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,2 +1,10 @@
 # basic file
 
+class World
+  attr_accessor :rows, :cols
+  
+  def initialize(rows=3, cols=3)
+    @rows = rows
+    @cols = cols
+  end
+end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,8 +1,20 @@
 # spec file
 
 require 'rspec'
+require_relative 'game_of_life.rb'
 
-describe "Game of life" do
-  
-  
+describe "Game of Life" do
+  context "World" do
+    subject { World.new }
+    
+    it "should create a new world object" do
+      expect(subject.class).to eq World
+    end
+    
+    it "should respond to proper methods" do
+      expect(subject).to respond_to(:rows)
+      expect(subject).to respond_to(:cols)
+    end
+    
+  end
 end
